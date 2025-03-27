f77b006e1f9243ea2b088222e872a09b5e390ce3
LittleFox-97 <shably2707@gmail.com>
2025-03-27 18:27:24 +0300

    modified:   src/requests/__init__.py 	modified:   src/requests/_internal_utils.py 	modified:   src/requests/certs.py 	modified:   src/requests/compat.py 	modified:   src/requests/help.py 	modified:   src/requests/packages.py

--- src/requests/_internal_utils.py
+++ src/requests/_internal_utils.py
@@ -1,50 +1,57 @@
-"""
-requests._internal_utils
-~~~~~~~~~~~~~~
-
-Provides utility functions that are consumed internally by Requests
-which depend on extremely few external helpers (such as compat)
-"""
+'''Provides utility functions that are consumed internally by Requests which depend on extremely few external helpers (such as compat).'''
 import re
 
-from .compat import builtin_str
+_VALID_HEADER_NAME_RE_BYTE = re.compile(rb'^[^:\s][^:\r\n]*$')
+_VALID_HEADER_NAME_RE_STR = re.compile(r'^[^:\s][^:\r\n]*$')
+_VALID_HEADER_VALUE_RE_BYTE = re.compile(rb'^\S[^\r\n]*$|^$')
+_VALID_HEADER_VALUE_RE_STR = re.compile(r'^\S[^\r\n]*$|^$')
 
-_VALID_HEADER_NAME_RE_BYTE = re.compile(rb"^[^:\s][^:\r\n]*$")
-_VALID_HEADER_NAME_RE_STR = re.compile(r"^[^:\s][^:\r\n]*$")
-_VALID_HEADER_VALUE_RE_BYTE = re.compile(rb"^\S[^\r\n]*$|^$")
-_VALID_HEADER_VALUE_RE_STR = re.compile(r"^\S[^\r\n]*$|^$")
-
-_HEADER_VALIDATORS_STR = (_VALID_HEADER_NAME_RE_STR, _VALID_HEADER_VALUE_RE_STR)
-_HEADER_VALIDATORS_BYTE = (_VALID_HEADER_NAME_RE_BYTE, _VALID_HEADER_VALUE_RE_BYTE)
+_HEADER_VALIDATORS_STR = (_VALID_HEADER_NAME_RE_STR,
+                          _VALID_HEADER_VALUE_RE_STR)
+_HEADER_VALIDATORS_BYTE = (_VALID_HEADER_NAME_RE_BYTE,
+                           _VALID_HEADER_VALUE_RE_BYTE)
 HEADER_VALIDATORS = {
     bytes: _HEADER_VALIDATORS_BYTE,
     str: _HEADER_VALIDATORS_STR,
 }
 
 
-def to_native_string(string, encoding="ascii"):
-    """Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
+def to_native_string(string: str | bytes, encoding: str = 'ascii') -> str:
     """
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        out = string.decode(encoding)
+    Convert a string-like object to a native string.
 
-    return out
+    Parameters
+    ----------
+    string : str | bytes
+        The string-like object to convert.
+    encoding : str, optional
+        The encoding to use to decode the string. Defaults to 'ascii'.
 
-
-def unicode_is_ascii(u_string):
-    """Determine if unicode string only contains ASCII characters.
-
-    :param str u_string: unicode string to check. Must be unicode
-        and not Python 2 `str`.
-    :rtype: bool
+    Returns
+    -------
+    str
+        A native string.
     """
-    assert isinstance(u_string, str)
+    return string if isinstance(string, str) else string.decode(encoding)
+
+
+def unicode_is_ascii(u_string: str) -> bool:
+    '''
+    Determine if a unicode string contains only ASCII characters.
+
+    Parameters
+    ----------
+    u_string : str
+        Unicode string to check. Must be unicode and not Python 2 `str`.
+
+    Returns
+    -------
+    bool
+        `True` if `u_string` only contains ASCII characters, `False` otherwise.
+    '''
     try:
-        u_string.encode("ascii")
-        return True
+        u_string.encode('ascii')
     except UnicodeEncodeError:
         return False
+    else:
+        return True
