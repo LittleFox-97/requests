b67780f2aeeac5207ab55e14577a10974bfe02d1
LittleFox-97 <shably2707@gmail.com>
2025-04-03 20:35:35 +0300

    modified:   .gitignore 	new file:   Pipfile 	modified:   pyproject.toml 	modified:   src/requests/__init__.py 	modified:   src/requests/__version__.py 	modified:   src/requests/_internal_utils.py 	modified:   src/requests/certs.py 	modified:   src/requests/packages.py

--- src/requests/__init__.py
+++ src/requests/__init__.py
@@ -1,13 +1,6 @@
-#   __
-#  /__)  _  _     _   _ _/   _
-# / (   (- (/ (/ (- _)  /  _)
-#          /
-
 """
-Requests HTTP Library
-~~~~~~~~~~~~~~~~~~~~~
-
 Requests is an HTTP library, written in Python, for human beings.
+
 Basic GET usage:
 
    >>> import requests
@@ -38,115 +31,13 @@ is at <https://requests.readthedocs.io>.
 :license: Apache 2.0, see LICENSE for more details.
 """
 
-import warnings
-
-import urllib3
-
-from .exceptions import RequestsDependencyWarning
-
-try:
-    from charset_normalizer import __version__ as charset_normalizer_version
-except ImportError:
-    charset_normalizer_version = None
-
-try:
-    from chardet import __version__ as chardet_version
-except ImportError:
-    chardet_version = None
-
-
-def check_compatibility(urllib3_version, chardet_version, charset_normalizer_version):
-    urllib3_version = urllib3_version.split(".")
-    assert urllib3_version != ["dev"]  # Verify urllib3 isn't installed from git.
-
-    # Sometimes, urllib3 only reports its version as 16.1.
-    if len(urllib3_version) == 2:
-        urllib3_version.append("0")
-
-    # Check urllib3 for compatibility.
-    major, minor, patch = urllib3_version  # noqa: F811
-    major, minor, patch = int(major), int(minor), int(patch)
-    # urllib3 >= 1.21.1
-    assert major >= 1
-    if major == 1:
-        assert minor >= 21
-
-    # Check charset_normalizer for compatibility.
-    if chardet_version:
-        major, minor, patch = chardet_version.split(".")[:3]
-        major, minor, patch = int(major), int(minor), int(patch)
-        # chardet_version >= 3.0.2, < 6.0.0
-        assert (3, 0, 2) <= (major, minor, patch) < (6, 0, 0)
-    elif charset_normalizer_version:
-        major, minor, patch = charset_normalizer_version.split(".")[:3]
-        major, minor, patch = int(major), int(minor), int(patch)
-        # charset_normalizer >= 2.0.0 < 4.0.0
-        assert (2, 0, 0) <= (major, minor, patch) < (4, 0, 0)
-    else:
-        warnings.warn(
-            "Unable to find acceptable character detection dependency "
-            "(chardet or charset_normalizer).",
-            RequestsDependencyWarning,
-        )
-
-
-def _check_cryptography(cryptography_version):
-    # cryptography < 1.3.4
-    try:
-        cryptography_version = list(map(int, cryptography_version.split(".")))
-    except ValueError:
-        return
-
-    if cryptography_version < [1, 3, 4]:
-        warning = "Old version of cryptography ({}) may cause slowdown.".format(
-            cryptography_version
-        )
-        warnings.warn(warning, RequestsDependencyWarning)
-
-
-# Check imported dependencies for compatibility.
-try:
-    check_compatibility(
-        urllib3.__version__, chardet_version, charset_normalizer_version
-    )
-except (AssertionError, ValueError):
-    warnings.warn(
-        "urllib3 ({}) or chardet ({})/charset_normalizer ({}) doesn't match a supported "
-        "version!".format(
-            urllib3.__version__, chardet_version, charset_normalizer_version
-        ),
-        RequestsDependencyWarning,
-    )
-
-# Attempt to enable urllib3's fallback for SNI support
-# if the standard library doesn't support SNI or the
-# 'ssl' library isn't available.
-try:
-    try:
-        import ssl
-    except ImportError:
-        ssl = None
-
-    if not getattr(ssl, "HAS_SNI", False):
-        from urllib3.contrib import pyopenssl
-
-        pyopenssl.inject_into_urllib3()
-
-        # Check cryptography version
-        from cryptography import __version__ as cryptography_version
-
-        _check_cryptography(cryptography_version)
-except ImportError:
-    pass
-
-# urllib3's DependencyWarnings should be silenced.
-from urllib3.exceptions import DependencyWarning
-
-warnings.simplefilter("ignore", DependencyWarning)
-
-# Set default logging handler to avoid "No handler found" warnings.
+import contextlib
 import logging
+import warnings
+from importlib.metadata import version
 from logging import NullHandler
+
+from urllib3.exceptions import DependencyWarning
 
 from . import packages, utils
 from .__version__ import (
@@ -161,15 +52,16 @@ from .__version__ import (
     __url__,
     __version__,
 )
-from .api import delete, get, head, options, patch, post, put, request
+from .api import delete, get, head, options, patch, post, put, request  # type: ignore
 from .exceptions import (
-    ConnectionError,
+    ConnectionError,  # noqa: A004
     ConnectTimeout,
     FileModeWarning,
     HTTPError,
     JSONDecodeError,
     ReadTimeout,
     RequestException,
+    RequestsDependencyWarning,
     Timeout,
     TooManyRedirects,
     URLRequired,
@@ -178,7 +70,134 @@ from .models import PreparedRequest, Request, Response
 from .sessions import Session, session
 from .status_codes import codes
 
+try:
+    import charset_normalizer
+except ImportError:
+    charset_normalizer_version = None
+else:
+    charset_normalizer_version = charset_normalizer.__version__
+
+try:
+    import chardet
+except ImportError:
+    chardet_version = None
+else:
+    chardet_version = chardet.__version__
+
+
+def _get_version(ver: str) -> tuple[int, ...]:
+    return tuple(map(int, ver))
+
+
+def check_compatibility(urllib3_ver: str, chardet_ver: str | None,
+                        normalizer_ver: str | None) -> None:
+    _compatibility_urllib3(urllib3_ver)
+    _compatibility_chardet(chardet_ver, normalizer_ver)
+
+
+def _compatibility_urllib3(urllib3_ver: str) -> None:
+    if 'dev' in urllib3_ver:
+        msg = 'Development versions of urllib3 are not supported!'
+        raise AssertionError(msg)
+    urllib3_version = _get_version(urllib3_ver)
+    if urllib3_version < (1, 21, 1):
+        msg = f'urllib3 >= 1.21.1 is required; you have {urllib3_ver}'
+        raise AssertionError(msg)
+
+
+def _compatibility_chardet(chardet_ver: str | None, normalizer_ver: str | None) -> None:
+    if chardet_ver:
+        chardet_version = _get_version(chardet_ver)
+        # chardet_version >= 3.0.2, < 6.0.0
+        assert (3, 0, 2) <= chardet_version < (6, 0, 0)  # noqa: S101
+    elif normalizer_ver:
+        normalizer_version = _get_version(normalizer_ver)
+        # charset_normalizer >= 2.0.0 < 4.0.0
+        assert (2, 0, 0) <= normalizer_version < (4, 0, 0)  # noqa: S101
+    else:
+        warnings.warn(
+            'Unable to find acceptable character detection dependency (chardet or charset_normalizer).',
+            RequestsDependencyWarning,
+            stacklevel=2,
+        )
+
+
+def _check_cryptography(cryptography_ver: str) -> None:
+    cryptography_version = _get_version(cryptography_ver)
+    # cryptography < 1.3.4
+
+    if cryptography_version < (1, 3, 4):
+        warning = f'Old version of cryptography ({cryptography_version}) may cause slowdown.'
+        warnings.warn(warning, RequestsDependencyWarning, stacklevel=2)
+
+
+urllib3_version = version('urllib3')
+
+# Check imported dependencies for compatibility.
+try:
+    check_compatibility(urllib3_version, chardet_version, charset_normalizer_version)
+except (AssertionError, ValueError):
+    warnings.warn(
+        f'urllib3 ({urllib3_version}) or chardet ({chardet_version})/charset_normalizer ({charset_normalizer_version})'
+        ' doesn`t match a supported version!',
+        RequestsDependencyWarning,
+        stacklevel=2,
+    )
+
+# Attempt to enable urllib3's fallback for SNI support
+# if the standard library doesn't support SNI or the
+# 'ssl' library isn't available.
+with contextlib.suppress(ImportError):
+    import ssl
+
+    from urllib3.contrib import pyopenssl
+
+    if not getattr(ssl, 'HAS_SNI', False):
+        pyopenssl.inject_into_urllib3()
+        from cryptography import __version__ as cryptography_version
+
+        _check_cryptography(cryptography_version)
+warnings.simplefilter('ignore', DependencyWarning)
+
 logging.getLogger(__name__).addHandler(NullHandler())
 
 # FileModeWarnings go off per the default.
-warnings.simplefilter("default", FileModeWarning, append=True)
+warnings.simplefilter('default', FileModeWarning, append=True)
+
+__all__ = [
+    'packages',
+    'utils',
+    '__author__',
+    '__author_email__',
+    '__build__',
+    '__cake__',
+    '__copyright__',
+    '__description__',
+    '__license__',
+    '__title__',
+    '__url__',
+    '__version__',
+    'delete',
+    'get',
+    'head',
+    'options',
+    'patch',
+    'post',
+    'put',
+    'request',
+    'ConnectTimeout',
+    'ConnectionError',
+    'HTTPError',
+    'JSONDecodeError',
+    'ReadTimeout',
+    'RequestException',
+    'Timeout',
+    'TooManyRedirects',
+    'URLRequired',
+    'PreparedRequest',
+    'Request',
+    'Response',
+    'Session',
+    'session',
+    'codes',
+]
--- src/requests/_internal_utils.py
+++ src/requests/_internal_utils.py
@@ -1,18 +1,12 @@
-"""
-requests._internal_utils
-~~~~~~~~~~~~~~
-
-Provides utility functions that are consumed internally by Requests
-which depend on extremely few external helpers (such as compat)
-"""
+'''Utility functions that are consumed internally by Requests which depend on extremely few external helpers.'''
 import re
 
 from .compat import builtin_str
 
-_VALID_HEADER_NAME_RE_BYTE = re.compile(rb"^[^:\s][^:\r\n]*$")
-_VALID_HEADER_NAME_RE_STR = re.compile(r"^[^:\s][^:\r\n]*$")
-_VALID_HEADER_VALUE_RE_BYTE = re.compile(rb"^\S[^\r\n]*$|^$")
-_VALID_HEADER_VALUE_RE_STR = re.compile(r"^\S[^\r\n]*$|^$")
+_VALID_HEADER_NAME_RE_BYTE = re.compile(rb'^[^:\s][^:\r\n]*$')
+_VALID_HEADER_NAME_RE_STR = re.compile(r'^[^:\s][^:\r\n]*$')
+_VALID_HEADER_VALUE_RE_BYTE = re.compile(rb'^\S[^\r\n]*$|^$')
+_VALID_HEADER_VALUE_RE_STR = re.compile(r'^\S[^\r\n]*$|^$')
 
 _HEADER_VALIDATORS_STR = (_VALID_HEADER_NAME_RE_STR, _VALID_HEADER_VALUE_RE_STR)
 _HEADER_VALIDATORS_BYTE = (_VALID_HEADER_NAME_RE_BYTE, _VALID_HEADER_VALUE_RE_BYTE)
@@ -22,29 +16,37 @@ HEADER_VALIDATORS = {
 }
 
 
-def to_native_string(string, encoding="ascii"):
-    """Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
-    """
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        out = string.decode(encoding)
+def to_native_string(string: str | bytes, encoding: str = 'ascii') -> builtin_str:
+    '''Convert a string to the native string type.
 
-    return out
+    Parameters
+    ----------
+    string : str | bytes
+        The string object to be converted.
+    encoding : str, optional
+        The encoding to use for decoding bytes, by default 'ascii'.
+
+    Returns
+    -------
+    str
+        The string in the native string type.
+    '''
+    return string if isinstance(string, str) else string.decode(encoding)
 
 
-def unicode_is_ascii(u_string):
-    """Determine if unicode string only contains ASCII characters.
+def unicode_is_ascii(u_string: str) -> bool:
+    '''
+    Determine if a unicode string only contains ASCII characters.
 
-    :param str u_string: unicode string to check. Must be unicode
-        and not Python 2 `str`.
-    :rtype: bool
-    """
-    assert isinstance(u_string, str)
-    try:
-        u_string.encode("ascii")
-        return True
-    except UnicodeEncodeError:
-        return False
+    Parameters
+    ----------
+    u_string : str
+        Unicode string to check. Must be a Python 3 `str`.
+
+    Returns
+    -------
+    bool
+        True if all characters are ASCII, False otherwise.
+    '''
+    assert isinstance(u_string, str)  # noqa: S101
+    return u_string.isascii()
